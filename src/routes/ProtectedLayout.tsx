353e61991a2bd64c079f49c1e62f12ddb95b1ba6
Vihanga <vihaaanga.mihiranga@gmail.com>
2025-03-07 17:57:02 +1100

    Integrate protected routing

--- src/routes/ProtectedLayout.tsx
+++ src/routes/ProtectedLayout.tsx
@@ -1,8 +1,18 @@
 import { Outlet } from 'react-router';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect } from 'react';
 
 const ProtectedLayout = () => {
-  
-  return <Outlet />;
+  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      loginWithRedirect();
+    }
+  }, [isAuthenticated, isLoading, loginWithRedirect]);
+
+
+  return isAuthenticated ? <Outlet /> : null;
 };
 
 export default ProtectedLayout;
