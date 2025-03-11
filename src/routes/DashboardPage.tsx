b6553136348848655c5b10145ee28edf29dd913b
Vihanga <vihaaanga.mihiranga@gmail.com>
2025-03-11 20:36:25 +1100

    Remove unused imports

--- src/routes/DashboardPage.tsx
+++ src/routes/DashboardPage.tsx
@@ -1,13 +1,11 @@
-import { Dock } from '@/components/Dock';
 import { AuthHeader } from '@/components/AuthHeader';
+import { MainContent } from '@/components/MainContent';
 
 const DashboardPage = () => {
   return (
     <>
       <AuthHeader tabName="Dashboard" />
-      <h2 className="flex justify-center text-2xl font-semibold p-4">Dashboard Page</h2>
-      <div className="flex items-center justify-center">{/* Content goes here */}</div>
-      <Dock />
+      <MainContent />
     </>
   );
 };
--- src/routes/GroupsPage.tsx
+++ src/routes/GroupsPage.tsx
@@ -1,12 +1,11 @@
 import { AuthHeader } from '@/components/AuthHeader';
+import { MainContent } from '@/components/MainContent';
 
 const GroupsPage = () => {
   return (
     <>
       <AuthHeader tabName="Groups" />
-      <main claassName="flex flex-col  items-center justify-center h-screen bg-gray-100">
-        <h1 className="text-2xl font-bold">Groups Page</h1>
-      </main>
+      <MainContent />
     </>
   );
 };
