76a68fa4c5870b272eb848f7e6bcb46b9768ceee
Vihanga <vihaaanga.mihiranga@gmail.com>
2025-03-11 20:25:17 +1100

    Added header for dashboard,groups and calendar tab with resposive for all screen sizes

--- src/routes/DashboardPage.tsx
+++ src/routes/DashboardPage.tsx
@@ -1,8 +1,10 @@
 import { Dock } from '@/components/Dock';
+import { AuthHeader } from '@/components/AuthHeader';
 
 const DashboardPage = () => {
   return (
     <>
+      <AuthHeader tabName="Dashboard" />
       <h2 className="flex justify-center text-2xl font-semibold p-4">Dashboard Page</h2>
       <div className="flex items-center justify-center">{/* Content goes here */}</div>
       <Dock />
