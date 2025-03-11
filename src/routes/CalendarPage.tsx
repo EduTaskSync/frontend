b919ead5d052f66a8e34150d88717aaf0cf83bb3
Vihanga <vihaaanga.mihiranga@gmail.com>
2025-03-11 20:36:25 +1100

    Remove unused imports

--- src/routes/CalendarPage.tsx
+++ src/routes/CalendarPage.tsx
@@ -1,13 +1,11 @@
-import { Dock } from '@/components/Dock';
 import { AuthHeader } from '@/components/AuthHeader';
+import { MainContent } from '@/components/MainContent';
 
 const CalendarPage = () => {
   return (
     <>
       <AuthHeader tabName="Calendar" />
-      <h2 className="flex justify-center text-2xl font-semibold p-4">Calendar Page</h2>
-      <div className="flex items-center justify-center">{/* Content goes here */}</div>
-      <Dock />
+      <MainContent />
     </>
   );
 };
