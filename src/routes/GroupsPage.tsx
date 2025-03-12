eb19554e3312ca1912f818fcb4be978ab91c8e89
Vihanga <vihaaanga.mihiranga@gmail.com>
2025-03-12 17:43:47 +1100

    Create group grid and populate with mockup group cards

--- src/routes/GroupsPage.tsx
+++ src/routes/GroupsPage.tsx
@@ -1,11 +1,15 @@
 import { AuthHeader } from '@/components/AuthHeader';
+import { GroupGrid } from '@/components/GroupGrid';
 import { MainContent } from '@/components/MainContent';
 
 const GroupsPage = () => {
   return (
     <>
       <AuthHeader tabName="Groups" />
-      <MainContent />
+      <MainContent>
+        <h1 className="text-xl sm:text-4xl md:text-2xl text-purple-500 font-heading font-bold mb-5">My Groups</h1>
+        <GroupGrid />
+      </MainContent>
     </>
   );
 };
