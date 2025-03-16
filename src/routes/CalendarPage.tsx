719b4fd33c1b93326071979c00356334d00b6956
Vihanga <vihaaanga.mihiranga@gmail.com>
2025-03-16 14:46:50 +1100

    stupid script

--- src/routes/CalendarPage.tsx
+++ src/routes/CalendarPage.tsx
@@ -1,11 +1,13 @@
 import { AuthHeader } from '@/components/AuthHeader';
 import { MainContent } from '@/components/MainContent';
+import { Dock } from '@/components/Dock';
 
 const CalendarPage = () => {
   return (
     <>
       <AuthHeader tabName="Calendar" />
       <MainContent />
+      <Dock />
     </>
   );
 };
