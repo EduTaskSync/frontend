f304d4169d5c59022f463fb00d93aaaa2636e166
cooi123 <calebooi@hotmail.com>
2025-03-17 01:48:01 +1100

    update prod url

--- src/constants/apiBaseUrl.ts
+++ src/constants/apiBaseUrl.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_USE_PROD_API === 'true' ? 'http://172.105.171.54:3000' : 'http://localhost:3000';
+  import.meta.env.VITE_USE_PROD_API === 'true' ? 'https://edutasksync.xyz' : 'http://localhost:3000';
